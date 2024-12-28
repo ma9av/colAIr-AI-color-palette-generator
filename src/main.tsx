89e65e1a217bf8b8aeca140783d2ca8cb0904f81
ma9av <manavpanchal42@gmail.com>
2024-12-28 11:52:51 +0530

    analytics-2

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Navbar from './components/Navbar.tsx'
-import { Analytics } from '@vercel/analytics/next'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 createRoot(document.getElementById('root')!).render(
 <div className='relative'>
-    <Analytics />
-    <SpeedInsights />
     <Navbar />
     <App />
 </div>
