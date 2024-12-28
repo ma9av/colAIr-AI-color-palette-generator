697ce0ba31063f666d9f81525042d106b3f2b71f
ma9av <manavpanchal42@gmail.com>
2024-12-28 11:58:35 +0530

    analytics-3

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import './index.css'
 import App from './App.tsx'
 import Navbar from './components/Navbar.tsx'
 import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 
 inject()
+injectSpeedInsights()
 
 createRoot(document.getElementById('root')!).render(
 <div className='relative'>
